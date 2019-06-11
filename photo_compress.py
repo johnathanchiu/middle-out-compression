48f41268402be47265b2decbb1d55f3ae0eb4762
john <johnathanchewy@gmail.com>
2019-06-11 00:09:50 -0700

    making sure decompress works

--- photo_compress.py
+++ photo_compress.py
@@ -67,9 +67,7 @@ def compress_image(image, file_name):
     pbar_mo = tqdm(range(1))
     for _ in pbar_mo:
         pbar_mo.set_description("running middle out compresssion")
-        mo_compressed = MiddleOut.byte_compression(compressed)
-        # if len(uncompressed) > 0:
-        #     size_mo, _ = EntropyReduction.bz2(compressed, 'a')
+        mo_compressed = MiddleOut.middle_out(compressed, debug=False)
 
     pbar = tqdm(range(1))
     for _ in pbar:
@@ -78,7 +76,6 @@ def compress_image(image, file_name):
 
     print(); print("size of array:", len(compressed) * 8); print("size of file after middleout",
                                                                  int(np.round(len(mo_compressed) / 8)))
-    # if len(uncompressed) > 0: print("stacked compression size after:", size_mo)
 
     return size, filename
 
