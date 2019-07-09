b0f41ca451634d445be23c17a5ebd8d6c21ab171
john <johnathanchewy@gmail.com>
2019-07-09 00:24:18 -0700

    split up values into partitions

--- compress_binary.py
+++ compress_binary.py
@@ -1,4 +1,4 @@
-from middleOut.utils import readFileBytes, writeFile, size_of_file, pad_stream, convertBin
+from middleOut.utils import readFileBytes, writeFile, size_of_file, pad_stream, convertBin, split_file
 from middleOut.MiddleOut import MiddleOut
 from middleOut.EntropyReduction import EntropyReduction
 
@@ -11,27 +11,34 @@ import time
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
-    ap.add_argument('-o', "--original", required=True, help="path to file")
-    ap.add_argument("-c", "--compressed", required=False, default="./", help="dir. to save file to")
+    ap.add_argument('-o', "--original", required=True, help="Path to file")
+    ap.add_argument("-c", "--compressed", required=False, default="/Users/johnathanchiu/Documents/MiddleOutTests/",
+                    help="Dir. to save file to")
     args = ap.parse_args()
     file_name = args.original
     compressed_file = args.compressed + os.path.splitext(os.path.basename(file_name))[0]
     bytes_of_file = readFileBytes(file_name)
     bytes_of_file = array.array('b', [b - 128 for b in bytes_of_file])
+    partitions = split_file(bytes_of_file, chunksize=80000)
     start_time = time.time()
 
-    pbar = tqdm(range(1))
-    for _ in pbar:
+    pbar = tqdm(partitions)
+    count, total_size = 1, 0
+    for p in pbar:
         pbar.set_description("running middle-out compression scheme")
-        # bytes_of_file = array.array('b', [b - 128 for b in list(EntropyReduction.lz4_compress(bytes_of_file))])
-        # bytes_of_file = array.array('b', EntropyReduction.bz2compressor(bytes_of_file))
-        mo_compressed = MiddleOut.middle_out(bytes_of_file, size=2)
+        # p_temp = array.array('b', [b - 128 for b in list(EntropyReduction.lz4_compress(bytes_of_file))])
+        # p_temp = array.array('b', EntropyReduction.bz2compressor(p))
+        # mo_compressed = MiddleOut.middle_out(p_temp, size=2)
+        mo_compressed = MiddleOut.middle_out(p, size=2)
         pad = pad_stream(len(mo_compressed))
         num_padded = convertBin(pad, bits=4)
-        writeFile(mo_compressed + ('0' * pad) + num_padded, fileName=compressed_file)
+        mo_compressed += ('0' * pad) + num_padded
+        writeFile(mo_compressed, fileName=compressed_file + '_' + str(count))
+        total_size += len(mo_compressed)
+        count += 1
 
     print("compression converges!")
-    compressed_size = size_of_file(compressed_file + '.bin')
+    compressed_size = total_size // 8
     print("size of resulting file:", compressed_size)
     print("compression percentage (compressed / original): ", compressed_size / size_of_file(file_name) * 100, "%")
     print("--- %s seconds ---" % (time.time() - start_time))
