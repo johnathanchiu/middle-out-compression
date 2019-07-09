43dc44417954cb8649863f11174eef477e7f9dd6
john <johnathanchewy@gmail.com>
2019-07-09 12:29:15 -0700

    finished compression base

--- middleOut/MiddleOut.py
+++ middleOut/MiddleOut.py
@@ -26,64 +26,62 @@ class MiddleOutUtils:
                 dictionary[partition] += 1
             count += 1
         large_occur = MiddleOutUtils.max_key(dictionary)
-        if debug:
-            print("largest occurring:", large_occur, ", ",
-                  "size of byte stream:", len(byte_stream))
         return large_occur
 
     @staticmethod
     def make_count(byte_stream):
         return Counter(byte_stream)
 
-    @staticmethod
-    def build_dict(byte_lib):
-        return {byte_lib: '0'}
-
 
 class MiddleOut:
 
+    SPLIT = 0.5
+
     @staticmethod
-    def middle_out_helper(byte_stream, compression_dict, debug=False):
-        if debug: print("current length:", len(byte_stream))
+    def decompress(values):
+        return
+
+
+    @staticmethod
+    def middle_out_helper(byte_stream, compression_dict):
         count, unc_count = 0, 0
         compressed = ''
         uncompressed = []
         while count < len(byte_stream):
-            compressor = 1
+            compressor = 2
             tup = tuple(byte_stream[count:count+compressor])
-            while tup in compression_dict and count+compressor < len(byte_stream)+1:
-                compressor += 1
-                tup = tuple(byte_stream[count:count+compressor])
-            if len(tup) > 1:
-                tup = tuple(byte_stream[count:count+compressor-1])
-            if tup not in compression_dict:
+            if tup != compression_dict:
                 compressed += '1'
                 uncompressed.append(byte_stream[count])
             else:
-                compressed += compression_dict[tup]
+                compressed += '0'
             count += len(tup)
-        if debug: print("bitstream length:", len(compressed), compressed)
         return compressed, uncompressed
 
     @staticmethod
     def byte_compression(values, size=2):
         if len(values) == 0:
             return ''
-        split, left, right = MiddleOut.splitter(values)
+        if len(values) < size:
+            while len(values) % size != 0:
+                values.append(0)
+            split, left, right = '', values, []
+        else:
+            split, left, right = MiddleOut.splitter(values)
         l_, r_ = MiddleOutUtils.build_library(left, size=size), MiddleOutUtils.build_library(right, size=size)
-        l_dict, r_dict = MiddleOutUtils.build_dict(l_), MiddleOutUtils.build_dict(r_)
-        comp_l, uncomp_l = MiddleOut.middle_out_helper(left, l_dict)
-        comp_r, uncomp_r = MiddleOut.middle_out_helper(right, r_dict)
-        return split + comp_l + MiddleOut.byte_compression(uncomp_l) + comp_r + MiddleOut.byte_compression(uncomp_r)
+        left_lib, right_lib = convertBin_list(l_, bits=8), convertBin_list(r_, bits=8)
+        comp_l, uncomp_l = MiddleOut.middle_out_helper(left, l_)
+        comp_r, uncomp_r = MiddleOut.middle_out_helper(right, r_)
+        stream_l = split + left_lib + comp_l
+        stream_r = right_lib + comp_r
+        return stream_l + MiddleOut.byte_compression(uncomp_l) + stream_r + MiddleOut.byte_compression(uncomp_r)
 
     @staticmethod
     def splitter(values):
         occurence = 0
         split_set = set([])
         occurence_dict = MiddleOutUtils.make_count(values)
-        print(occurence_dict)
-        occurence_dict = MiddleOutUtils.max_key(occurence_dict)
-        while occurence / len(values) < 0.5:
+        while occurence / len(values) < MiddleOut.SPLIT:
             largest = MiddleOutUtils.max_key(occurence_dict)
             split_set.add(largest)
             occurence += occurence_dict[largest]
@@ -94,7 +92,6 @@ class MiddleOut:
     def branch_tree(values, left_tree_values):
         split = ''
         right, left = array.array('b', []), array.array('b', [])
-        left_tree_values = set(left_tree_values)
         for v in values:
             if v in left_tree_values:
                 split += '0'
@@ -106,6 +103,8 @@ class MiddleOut:
 
     @staticmethod
     def middle_out(coefficients, size=2):
-        while len(coefficients) % size != 0:
-            coefficients.append(0)
         return MiddleOut.byte_compression(coefficients, size=size)
+
+    @staticmethod
+    def middle_out_decompress(compressed, size=2):
+        return
