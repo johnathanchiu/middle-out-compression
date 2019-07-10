1bb0f7b75f2e8739aa8102661d7f5387ec2933cb
john <johnathanchewy@gmail.com>
2019-07-10 07:39:26 -0700

    added binary middle out

---
+++ middleOut/BinaryMiddleOut.py
@@ -0,0 +1,16 @@
+from collections import Counter
+from middleOut.MiddleOut import MiddleOutUtils
+
+
+class BinaryMiddleOut:
+
+    @staticmethod
+    def compressor(values):
+        leading = MiddleOutUtils.max_key(Counter(values))
+        move_to_back = ''
+        for i in values:
+            if i == leading:
+                move_to_back += leading
+            else:
+                move_to_back += i
+        return
