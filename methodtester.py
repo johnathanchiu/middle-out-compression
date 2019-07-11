c6e2b2b6a12e413ef93302e251c4c8d998fbc144
john <johnathanchewy@gmail.com>
2019-07-10 17:18:25 -0700

    update method tester

--- methodtester.py
+++ methodtester.py
@@ -26,30 +26,31 @@ class TestMiddleOut:
             print("bitsets are the same")
 
     @staticmethod
-    def generate_random(bitset_size, set_seed=False, seed=0):
-        if (set_seed):
+    def generate_random(size, seeding=False, seed=0):
+        if (seeding):
             np.random.seed(seed)
-        return np.random.randint(100, size=bitset_size)
+        return np.random.randint(100, size=size)
 
     @staticmethod
     def run_middleout(bytes):
-        return MiddleOut.middle_out(bytes, size=3, debug=True)
+        return MiddleOut.middle_out(bytes, size=3, debug=False)
 
     @staticmethod
     def run_middelout_decomp(bits):
-        return MiddleOut.middle_out_decompress(bits, debug=True)
+        return MiddleOut.middle_out_decompress(bits, debug=False)
 
 if __name__ == '__main__':
     start_time = time.time()
     # test = [1, 2, 1, 2, 4, 5, 6, 7, 8, 9, 10]
-    test = [1, 5, 2, 3, 6, 5, 2, 4, 2, 3, 4, 6, 1, 2, 1, 2, 1, 2, 1, 2]
+    # test = [1, 5, 2, 3, 6, 5, 2, 4, 2, 3, 4, 6, 1, 2, 1, 2, 1, 2, 1, 2]
     # test = [255, 216, 255, 224, 0, 16, 74, 70, 73, 70]
-    test[:] = [b-128 for b in test]
+    test = TestMiddleOut.generate_random(10000)
+    # test[:] = [b-128 for b in test]
     print("size before middleout", len(test), "(bytes)", ", ", len(test) * 8, "(bits)")
     c = TestMiddleOut.run_middleout(test)
     print("size of middleout", len(c))
     de = TestMiddleOut.run_middelout_decomp(c)
-    print("decompressed", de, "original", test)
+    print("decompressed", de); print("original", test)
     TestMiddleOut.check_differences(test, de)
     print("--- %s seconds ---" % (time.time() - start_time))
 
