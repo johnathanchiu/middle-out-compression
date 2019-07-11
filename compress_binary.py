c8fa4e4623f4cec90462e42dae885613623f70cc
john <johnathanchewy@gmail.com>
2019-07-11 00:52:30 -0700

    self store files

--- compress_binary.py
+++ compress_binary.py
@@ -12,11 +12,12 @@ import time
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
     ap.add_argument('-o', "--original", required=True, help="Path to file")
-    ap.add_argument("-c", "--compressed", required=False, default="/Users/johnathanchiu/Documents/MiddleOutTests/",
+    ap.add_argument("-c", "--compressed", required=False, default="./",
                     help="Dir. to save file to")
     args = ap.parse_args()
     file_name = args.original
-    compressed_file = args.compressed + os.path.splitext(os.path.basename(file_name))[0]
+    compressed_file = args.compressed + os.path.splitext(os.path.basename(file_name))[0] + \
+                      os.path.splitext(os.path.basename(file_name))[1]
     bytes_of_file = readFileBytes(file_name)
     bytes_of_file = array.array('b', [b - 128 for b in bytes_of_file])
     partitions = split_file(bytes_of_file, chunksize=len(bytes_of_file))
@@ -28,12 +29,11 @@ if __name__ == '__main__':
         pbar.set_description("running middle-out compression scheme")
         # p_temp = array.array('b', [b - 128 for b in list(EntropyReduction.lz4_compress(bytes_of_file))])
         # p_temp = array.array('b', EntropyReduction.bz2compressor(p))
-        # mo_compressed = MiddleOut.middle_out(p_temp, size=2)
         mo_compressed = MiddleOut.middle_out(p, size=2)
         pad = pad_stream(len(mo_compressed))
         num_padded = convertBin(pad, bits=4)
         mo_compressed += ('0' * pad) + num_padded
-        writeFile(mo_compressed, fileName=compressed_file + '_' + str(count))
+        writeFile(mo_compressed, fileName=compressed_file)
         total_size += len(mo_compressed)
         count += 1
 
--- decompress_binary.py
+++ decompress_binary.py
@@ -11,11 +11,10 @@ import time
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
     ap.add_argument('-c', "--compressed", required=True, help="Path to compressed file")
-    ap.add_argument('-o', "--path", required=False, default='./', help="Path to save the decompressed file")
-    ap.add_argument('-d', "--decompressed", required=True, help="Extension of original file with preceding '.'")
+    ap.add_argument('-p', "--path", required=False, default='./', help="Path to save the decompressed file")
     args = ap.parse_args()
-    compressed_file, decompressed_ext = args.compressed, args.decompressed
-    decompressed = os.path.splitext(os.path.basename(compressed_file))[0] + decompressed_ext
+    compressed_file = args.compressed
+    decompressed = args.path + os.path.splitext(os.path.basename(compressed_file))[0]
     start_time = time.time()
 
     pbar = tqdm(range(1))
@@ -25,5 +24,6 @@ if __name__ == '__main__':
         decomp = array.array('B', [b+128 for b in array.array('b', MiddleOut.middle_out_decompress(bitstream))])
         writeFileBytes(decomp, decompressed)
 
+    print("file saved to:", decompressed)
     print("decompression converges!")
     print("--- %s seconds ---" % (time.time() - start_time))
